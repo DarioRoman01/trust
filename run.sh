4291f8530c851855829f2685488d74c467f20102
Haizza1 <darioroman2001@gmail.com>
2021-05-17 11:10:13 -0400

    create state struct.

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cargo b --release
+ext=$?
+echo "$ext"
+if [[ $ext -ne 0]]; then
+        exit $ext
+fi
 sudo setcap cap_net_admin=eip $CARGO_TARGET_DIR/release/trust
 $CARGO_TARGET_DIR/release/trust &
 pid=$!
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use std::io;
+use std::collections::HashMap;
+use std::net::Ipv4Addr;
+mod tcp;
+
+
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+struct Quad {
+	src: (Ipv4Addr, u16),
+	dst: (Ipv4Addr, u16),
+
+}
 
 fn main() -> io::Result<()> {
+	let mut connections: HashMap<Quad, tcp::State> = Default::default();
 	let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
 	let mut buff = [0u8; 1504];
 
 	loop {
 		let nbytes = nic.recv(&mut buff[..])?;
-		let eth_flags = u16::from_be_bytes([buff[0], buff[1]]);
+		let _eth_flags = u16::from_be_bytes([buff[0], buff[1]]);
 		let eth_proto = u16::from_be_bytes([buff[2], buff[3]]);
 
 		if eth_proto != 0x0800 {
@@ -15,18 +27,30 @@ fn main() -> io::Result<()> {
 		}
 
 		match etherparse::Ipv4HeaderSlice::from_slice(&buff[4..nbytes]) {
-			Ok(p) => {
-				let src = p.source_addr();
-				let dst = p.destination_addr();
-				let proto = p.protocol();
-				eprintln!("{} => {} {}b of protocol {} ", src, dst, proto, p.payload_len())
+			Ok(iph) => { // ip header
+				let src = iph.source_addr();
+				let dst = iph.destination_addr();
+				if iph.protocol() != 0x06 {
+					// not tcp  
+					continue;
+				}
+				
+				match etherparse::TcpHeaderSlice::from_slice(&buff[4+iph.slice().len()..]) {
+					Ok(tcph) => { // tcp header
+						let datai = 4 + iph.slice().len() + tcph.slice().len();
+						connections.entry(Quad {
+							src: (src, tcph.source_port()),
+        			dst: (dst, tcph.destination_port()),
+						}).or_default().on_packet(iph, tcph, &buff[datai..nbytes]);
+					},
+					Err(e) => {
+						eprintln!("ignoring weird package {:?}", e);
+					}
+				}
 			},
-			
 			Err(e) => {
 				eprintln!("ignoring weird package {:?}", e);
 			}
 		};
 	};
-
-	Ok(())
 }
