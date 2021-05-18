90eac0b65cefd7d50d1c2a70d435c85e6fea009d
Haizza1 <darioroman2001@gmail.com>
2021-05-18 09:19:16 -0400

    implmententing response for packets.

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,15 @@ use std::collections::HashMap;
 use std::net::Ipv4Addr;
 mod tcp;
 
-
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
 struct Quad {
 	src: (Ipv4Addr, u16),
 	dst: (Ipv4Addr, u16),
-
 }
 
 fn main() -> io::Result<()> {
 	let mut connections: HashMap<Quad, tcp::State> = Default::default();
-	let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+	let mut nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
 	let mut buff = [0u8; 1504];
 
 	loop {
@@ -31,17 +29,19 @@ fn main() -> io::Result<()> {
 				let src = iph.source_addr();
 				let dst = iph.destination_addr();
 				if iph.protocol() != 0x06 {
-					// not tcp  
+					// not tcp
 					continue;
 				}
-				
-				match etherparse::TcpHeaderSlice::from_slice(&buff[4+iph.slice().len()..]) {
+
+				match etherparse::TcpHeaderSlice::from_slice(&buff[4+iph.slice().len()..nbytes]) {
 					Ok(tcph) => { // tcp header
 						let datai = 4 + iph.slice().len() + tcph.slice().len();
 						connections.entry(Quad {
 							src: (src, tcph.source_port()),
 							dst: (dst, tcph.destination_port()),
-						}).or_default().on_packet(iph, tcph, &buff[datai..nbytes]);
+						})
+						.or_default()
+						.on_packet(&mut nic, iph, tcph, &buff[datai..nbytes])?;
 					},
 					Err(e) => {
 						eprintln!("ignoring weird package {:?}", e);
--- src/tcp.rs
+++ src/tcp.rs
@@ -1,25 +1,81 @@
-pub struct State {}
+use std::io;
+
+pub enum State {
+	Closed,
+	Listen,
+	SynRcvd,
+	Estab,
+}
 
 impl Default for State {
 	fn default() -> Self {
-			State{}
+			//State::Closed
+			State::Listen
 	}
 }
 
 impl State {
 	pub fn on_packet<'a>(
 		&mut self,
+		nic: &mut tun_tap::Iface,
 		iph: etherparse::Ipv4HeaderSlice<'a>, 
 		tcph: etherparse::TcpHeaderSlice<'a>, 
-		data: &'a [u8],
-	) {
-		eprintln!(
-			"{}:{} => {}:{},  {}b of tcp", 
-			iph.source_addr(), 
-			tcph.source_port(),
-			iph.destination_addr(),
-			tcph.destination_port(),
-			data.len(),
-		);
+		_data: &'a [u8],
+	) -> io::Result<usize> {
+		let mut buff = [0u8; 1500];
+		match *self {
+			State::Closed => {
+				return Ok(0);
+			}
+
+			State::Listen => {
+				if !tcph.syn() {
+					// only exptected syn packet
+					return Ok(0);
+				}
+
+				// need to start establishing a connection
+				let mut syn_ack = etherparse::TcpHeader::new(
+					tcph.destination_port(), 
+					tcph.source_port(), 
+					0,
+					0,
+				);
+
+				syn_ack.syn = true;
+				syn_ack.ack = true;
+
+				let ip = etherparse::Ipv4Header::new(
+					syn_ack.header_len(), 
+					64, 
+					etherparse::IpTrafficClass::Tcp, 
+					[
+						iph.destination()[0],
+						iph.destination()[1],
+						iph.destination()[2],
+						iph.destination()[3],
+					],
+					[
+						iph.source()[0],
+						iph.source()[1],
+						iph.source()[2],
+						iph.source()[3],
+					],
+				);
+				
+				// write out the headers
+				let unwritten = {
+					let mut unwritten = &mut buff[..];
+					ip.write(&mut unwritten);
+					syn_ack.write(&mut unwritten);
+					unwritten.len()
+				};
+
+				nic.send(&buff[..unwritten])
+			}
+
+			State::SynRcvd => { return Ok(0)}
+			State::Estab => { return Ok(0)}
+		}
 	}
 }
